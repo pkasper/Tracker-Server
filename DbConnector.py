8cf912d31dd8d872aacb663edcc24ec090a3c0cc
pkasper <patrick.kasper@student.tugraz.at>
2015-03-05 16:51:05 +0100

    mysql fixes
    the server will now try to reconnect after a connection has gone away

--- DbConnector.py
+++ DbConnector.py
@@ -1,24 +1,47 @@
 import pymysql
-
+import time
 
 class DbConnector:
+    db_cursor = None
+    db_connection = None
+    db_host = 'localhost'
 
-    def __init__(self):
-        self.db_host = 'localhost'
-        self.db_connection = pymysql.connect(host=self.db_host,
+    def connect(self):
+        try:
+            self.db_connection = pymysql.connect(host=self.db_host,
                                              port=3306,
                                              user='root',
                                              passwd='master',
                                              db='wikigame',
                                              charset='utf8')
-        self.db_cursor = self.db_connection.cursor(pymysql.cursors.DictCursor)
+            self.db_cursor = self.db_connection.cursor(pymysql.cursors.DictCursor)
+            print("Database Connection Established")
+        except pymysql.err.OperationalError as _error:
+            print("SQL ERROR: " + str(_error))
+            time.sleep(5)
+            self.connect()
+
+    def alivecheck(self):
+        try:
+            self.db_cursor.execute("SELECT 1", ())
+            return True
+        except pymysql.err.OperationalError as _error:
+            print("SQL ERROR: " + str(_error))
+            self.connect()
+            time.sleep(2)
+            return self.alivecheck()
+
+    def __init__(self):
+        self.connect()
 
     def __exit__(self):
         self.db_cursor.close()
         self.db_connection.close()
 
     def execute(self, _statement, _args, _type):
-        self.db_cursor.execute(_statement, _args)
+        if self.alivecheck():
+            self.db_cursor.execute(_statement, _args)
+
 
         if _type == "SELECT":
             return self.db_cursor.fetchall()
